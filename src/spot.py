56d23504314bc3fe54fd53be8156c80c4ff6fe39
Grayson Poon <grayson.a.poon@gmail.com>
2022-12-20 01:01:30 -0800

    create spot class; spot representing one slot on the board

---
+++ src/spot.py
@@ -0,0 +1,24 @@
+from piece import Piece
+
+class Spot:
+  def __init__(self, x: int, y: int, piece: Piece | None) -> None:
+    self._x = x
+    self._y = y
+    self._piece = piece
+
+  @property
+  def x(self) -> int:
+    return self._x
+
+  @property
+  def y(self) -> int:
+    return self._y
+
+  @property
+  def piece(self) -> Piece:
+    return self._piece
+  
+  @piece.setter
+  def setPiece(self, piece: Piece) -> None:
+    self._piece = piece
+
